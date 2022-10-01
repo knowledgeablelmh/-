97a459698d3b29b6bfad6121cb281c65d8ec3018
knowledgeablelmh <83534987+knowledgeablelmh@users.noreply.github.com>
2022-10-01 10:22:18 +0800

    Update JDwskey.js

--- JDwskey/JDwskey.js
+++ JDwskey/JDwskey.js
@@ -10,7 +10,7 @@ var CookieValue = ck.match(/wskey=.+?;/)
 
 data=JSON.parse($response.body)
     data = data.userInfoSns.unickName
-    url = '%E4%BD%99%E6%B8%A9wskey%E8%8E%B7%E5%8F%96%E6%8F%90%E9%86%92%E6%82%A8'
+    url = '%E6%81%AD%E5%96%9C%E6%82%A8'
 
 
 

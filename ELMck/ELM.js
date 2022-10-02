d46bb8998c415aa994742a76b79f94bebd68f103
knowledgeablelmh <83534987+knowledgeablelmh@users.noreply.github.com>
2022-10-02 15:49:46 +0800

    Update ELM.js

--- ELMck/ELM.js
+++ ELMck/ELM.js
@@ -5,15 +5,13 @@
  * https://raw.githubusercontent.com/a358594784/yuwen.github.io/main/ywwskey.js
  */
 
-ck =$request.headers.Cookie
+ck = $request.headers.Cookie
 var CookieValue = ck
 
-data=JSON.parse($response.body)
-    data = data.userInfoSns.unickName
-    url = '%E6%81%AD%E5%96%9C%E6%82%A8'
-
-
+data = JSON.parse($response.body)
+data = data.userInfoSns.unickName
+url = '%E6%81%AD%E5%96%9C%E6%82%A8'
 
 console.log(ck)
-$notify(decodeURI(url), "饿了么ck获取成功", CookieValue);
-    $done();
+$.notify(decodeURI(url), "饿了么ck获取成功", CookieValue);
+$.done();

218ad980f15cab99bc3ce5aa37c7a6e26eb283c3
knowledgeablelmh <83534987+knowledgeablelmh@users.noreply.github.com>
2022-10-03 12:16:08 +0800

    Update ELM.js

--- ELMck/ELM.js
+++ ELMck/ELM.js
@@ -3,6 +3,7 @@
  * @fileoverview Example to compose HTTP request
  * and handle the response.
  */
+
 ck =$request.headers.Cookie
 var CookieValue = ck.match(/isg=.+?;/)
 
@@ -11,7 +12,6 @@ data=JSON.parse($response.body)
     url = '%E6%81%AD%E5%96%9C%E6%82%A8'
 
 
-
 console.log(ck)
-$notify(decodeURI(url), "京东wskey获取成功", CookieValue);
+$notify("饿了么ck获取成功", data + CookieValue);
     $done();

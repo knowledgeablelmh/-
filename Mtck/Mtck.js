f0b47a1144f64ff217a0e47012a31e25ef040e91
knowledgeablelmh <83534987+knowledgeablelmh@users.noreply.github.com>
2022-10-06 01:25:25 +0800

    Update Mtck.js

--- Mtck/Mtck.js
+++ Mtck/Mtck.js
@@ -1,12 +1,13 @@
-ck =$request.headers.Cookie
+ck = $request.headers.Cookie
 var CookieValue = ck.match(/token=.+?;/)
 
 
-data=JSON.parse($request.url)
-    data = data.url.userid
-    url = '%E6%81%AD%E5%96%9C%E6%82%A8'
+data = url.split("userid=")[1]
 
+
+
+url = '%E6%81%AD%E5%96%9C%E6%82%A8'
 
 console.log(ck)
-$notify(decodeURI(url), "美团ck获取成功", "userid="+data+";"+CookieValue);
-    $done();
+$notify(decodeURI(url), "美团ck获取成功", "userid=" + data + ";" + CookieValue);
+$done();

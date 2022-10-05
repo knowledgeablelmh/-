3ac7e8355573134ecae815fa6bae42649a4eee6d
knowledgeablelmh <83534987+knowledgeablelmh@users.noreply.github.com>
2022-10-06 00:52:42 +0800

    Update Mtck.js

--- Mtck/Mtck.js
+++ Mtck/Mtck.js
@@ -1,12 +1,12 @@
 ck =$request.headers.Cookie
-var CookieValue = ck.match(/wskey=.+?;/)
+var CookieValue = ck.match(/token=.+?;/)
 
-data=JSON.parse($response.body)
-    data = data.userInfoSns.unickName
+
+data=JSON.parse($request)
+    data = data.url.split("userId=")[1]
     url = '%E6%81%AD%E5%96%9C%E6%82%A8'
 
 
-
 console.log(ck)
-$notify(decodeURI(url), "京东wskey获取成功", "pin="+data+";"+CookieValue);
+$notify(decodeURI(url), "美团ck获取成功", "userid="+data+";"+CookieValue);
     $done();

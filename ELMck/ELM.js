544e6bac654892ce3dcfc9396e706d817693e1e4
knowledgeablelmh <83534987+knowledgeablelmh@users.noreply.github.com>
2022-10-05 22:39:39 +0800

    Update ELM.js

--- ELMck/ELM.js
+++ ELMck/ELM.js
@@ -12,5 +12,5 @@ data=JSON.parse($request.body)
     url = '%E6%81%AD%E5%96%9C%E6%82%A8'
 
 console.log(ck)
-$notify("饿了么ck获取成功", "USERID="+data+";"+CookieValue);
+$notify(decodeURI(url), ("饿了么ck获取成功", "USERID="+data+";"+CookieValue);
     $done();
